7ecb52e25270385948efde2d1cc225c83b31738d
Anton <93189104+DrSveras@users.noreply.github.com>
2022-01-06 16:55:18 +0300

    Add files via upload

---
+++ Calculator/src/com/company/calculator/Main.java
@@ -0,0 +1,15 @@
+package com.company.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scr = new Scanner(System.in);
+
+int nam1 = scr.nextInt();
+int nam2 = scr.nextInt();
+
+
+    }
+}
